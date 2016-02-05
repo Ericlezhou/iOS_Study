2a2ed5718736e5f69a750c87c106996d27bb895b
nexuszhoule <nexuszhoule@gmail.com>
2016-02-05 20:49:43 +0800

    initial the  project

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by 周乐 on 16/2/5.
+//  Copyright © 2016年 ericlezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
