c3ffac0d10cbb941b1ad4ca1d546ac97e0d5b7dc
nexuszhoule <nexuszhoule@gmail.com>
2016-02-27 23:51:59 +0800

    initial

---
+++ blockTest/blockTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blockTest
+//
+//  Created by 周乐 on 16/2/27.
+//  Copyright © 2016年 ericlezhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ blockTest/blockTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  blockTest
+//
+//  Created by 周乐 on 16/2/27.
+//  Copyright © 2016年 ericlezhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
