636fcfcd93d2a21e55b2bcc99c1e8f30e2fbc04e
eric <nexuszhoule@gmail.com>
2017-04-26 11:09:15 +0800

    start try lottie

---
+++ LottieDemo/LottieDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LottieDemo
+//
+//  Created by Eric on 2017/4/26.
+//  Copyright © 2017年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
